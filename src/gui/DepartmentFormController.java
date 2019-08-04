b3bf203ed934ce1bd192081580530db383294b76
Marcelo Silveira <marcelingsm@gmail.com>
2019-08-04 13:39:57 -0300

    update and remove

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -6,12 +6,17 @@
 package gui;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,6 +25,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 /**
@@ -31,6 +37,8 @@ public class DepartmentFormController implements Initializable {
     private Department entity;
     
     private DepartmentService service;
+    
+    private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
     
     @FXML
     private TextField txtId;
@@ -47,58 +55,97 @@ public class DepartmentFormController implements Initializable {
     @FXML
     private Button btCancel;
     
-    public void setDepartment(Department entity){
+    public void setDepartment(Department entity) {
         this.entity = entity;
     }
-    public void setDepartmentService(DepartmentService service){
+
+    public void setDepartmentService(DepartmentService service) {
         this.service = service;
     }
     
+    public void subscribeDataChangeListener(DataChangeListener listener) {
+        dataChangeListeners.add(listener);
+    }
+    
     @FXML
-    public void onBtSaveAction(ActionEvent event){
+    public void onBtSaveAction(ActionEvent event) {
         if (entity == null) {
             throw new IllegalStateException("entity was null");
         }
         if (service == null) {
             throw new IllegalStateException("Service was null");
         }
-        try{
+        try {
             
-        entity = getFormData();
-        service.saveOrUpdate(entity);
-        Utils.currentStage(event).close();
-        
-        }catch(DbException e){
-            Alerts.showAlert("Error saving object", null, e.getMessage() , Alert.AlertType.ERROR);
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            notifyDataChangeListener();
+            Utils.currentStage(event).close();
+            
         }
+        catch (ValidationException e){
+            setErrorMessages(e.getErrors());
         }
+        catch (DbException e) {
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
     
-    private Department getFormData(){
+    private Department getFormData() {
         Department obj = new Department();
+        
+        ValidationException exception = new ValidationException("Validation error");
+        
         obj.setId(Utils.tryParseToInt(txtId.getId()));
+        
+        if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+            exception.addError("name", "Field can't be empty");
+        }
         obj.setName(txtName.getText());
+        
+        if (exception.getErrors().size() > 0) {
+            throw exception;
+        }
         return obj;
     }
     
     @FXML
-    public void onBtCancelAction(ActionEvent event){
+    public void onBtCancelAction(ActionEvent event) {
         Utils.currentStage(event).close();
     }
-
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initializeNodes();
     }
     
-    private void initializeNodes(){
+    private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
         Constraints.setTextFieldMaxLength(txtName, 30);
     }
     
-    public void updateFormData(){
-        if(entity == null) throw new IllegalStateException();
+    public void updateFormData() {
+        if (entity == null) {
+            throw new IllegalStateException();
+        }
         
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
     }
+
+    public void notifyDataChangeListener() {
+        for (DataChangeListener listener : dataChangeListeners) {
+            listener.onDataChanged();
+        }
+        
+    }
+
+    private void setErrorMessages(Map<String, String> errors) {
+        Set<String> fields = errors.keySet();
+        if (fields.contains("name")) {
+            labelErrorName.setText(errors.get("name"));
+        }
+    }
+    
 }
+
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -5,12 +5,16 @@
  */
 package gui;
 
+import db.DbIntegrityException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -20,6 +24,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -34,7 +40,7 @@ import model.services.DepartmentService;
  *
  * @author USER
  */
-public class DepartmentListController implements Initializable {
+public class DepartmentListController implements Initializable, DataChangeListener {
 
     private DepartmentService service;
 
@@ -46,6 +52,12 @@ public class DepartmentListController implements Initializable {
 
     @FXML
     private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private TableColumn<Department, Department> tableColumnEDIT;
+
+    @FXML
+    private TableColumn<Department, Department> tableColumnREMOVE;
 
     @FXML
     private Button btNew;
@@ -83,7 +95,8 @@ public class DepartmentListController implements Initializable {
         List<Department> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
-
+        initEditButtons();
+        initRemoveButtons();
     }
 
     private void createDialogForm(Department obj, String absoluteName, Stage parentStange) {
@@ -94,8 +107,9 @@ public class DepartmentListController implements Initializable {
             DepartmentFormController controller = loader.getController();
             controller.setDepartment(obj);
             controller.setDepartmentService(new DepartmentService());
+            controller.subscribeDataChangeListener(this);
             controller.updateFormData();
-            
+
             Stage dialogStage = new Stage();
             dialogStage.setTitle("Enter Department data");
             dialogStage.setScene(new Scene(pane));
@@ -106,6 +120,65 @@ public class DepartmentListController implements Initializable {
 
         } catch (IOException e) {
             Alerts.showAlert("IO Exception", "ERRO view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    @Override
+    public void onDataChanged() {
+        updateTableView();
+    }
+
+    private void initEditButtons() {
+        tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("edit");
+
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createDialogForm(
+                                obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
+            }
+        });
+
+    }
+
+    private void initRemoveButtons() {
+        tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnREMOVE.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("remove");
+
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(event -> removeEntity(obj));
+            }
+        });
+    }
+
+    private void removeEntity(Department obj) {
+        Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
+        if (result.get() == ButtonType.OK) {
+            if (service == null) {
+                service.remove(obj);
+            }
+            try {
+                service.remove(obj);
+                updateTableView();
+            } catch (DbIntegrityException e) {
+                Alerts.showAlert("Error removing object", null, e.getMessage(), Alert.AlertType.ERROR);
+            }
         }
     }
 }
